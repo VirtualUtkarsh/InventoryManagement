2c86723597995e0971558c4127335cd23309379c
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2025-07-02 21:02:00 +0530

    Update auth.js
    commented 16th line as it was cauing error no register due to double response sending

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -13,7 +13,7 @@ router.post('/register', [
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ], async (req, res) => {
     console.log('REGISTER ROUTE HIT');
-    res.send('ok');
+    // res.send('ok');
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json({ message: errors.array()[0].msg })
